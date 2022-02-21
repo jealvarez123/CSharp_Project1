e2d3c1e70f3321b62415722cafaf0734d42960dc
Jaime Alvarez <jealvarez123@gmail.com>
2022-02-21 15:06:55 -0600

    First C# code

---
+++ Project1.cs
@@ -0,0 +1,33 @@
+using System;
+
+namespace ConsoleCreatures
+{
+  class Program
+  {
+    static void Main()
+    {//This code will be used to create a creature
+      Console.WriteLine(" .-.");
+      Console.WriteLine("(o o)");
+      Console.WriteLine("| O |");
+      Console.WriteLine("|   |");
+      Console.WriteLine("'~~~'");
+      Console.WriteLine("Lil Spooky");
+      Console.WriteLine("c_     __");
+      Console.WriteLine("  \\\\__|__|__");
+      Console.WriteLine("    \\( ^_^)");
+      Console.WriteLine("      > ~  > ");
+      Console.WriteLine("     /    /\\\\");
+      Console.WriteLine("    /    /  \\\\");
+      Console.WriteLine("    \\   )   c/");
+      Console.WriteLine("   /   /");
+      Console.WriteLine("  /   /|");
+      Console.WriteLine(" (  (  \\");
+      Console.WriteLine(" |  |  \\");
+      Console.WriteLine(" | / \\  )");
+      Console.WriteLine(" | |  ) |");
+      Console.WriteLine(" /  )(_/");
+      Console.WriteLine("(_ /");
+      Console.ReadLine();
+     }
+  }
+}
